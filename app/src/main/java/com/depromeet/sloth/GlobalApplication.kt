bc037c17bbd43df51d248ee35b84b6ec31f1e3c1
easyhooon <51016231+easyhooon@users.noreply.github.com>
2021-10-11 22:04:35 +0900

    #4 feat: 구글 로그인을 위한 설정 적용 및 코드 추가

--- app/src/main/java/com/depromeet/sloth/GlobalApplication.kt
+++ app/src/main/java/com/depromeet/sloth/GlobalApplication.kt
@@ -1,12 +1,15 @@
 package com.depromeet.sloth
 
 import android.app.Application
+import android.util.Log
 import com.kakao.sdk.common.KakaoSdk
+import com.kakao.sdk.common.util.Utility
 
 class GlobalApplication : Application() {
     override fun onCreate() {
         super.onCreate()
 
-        KakaoSdk.init(this, BuildConfig.KAKAO_API_KEY);
+        KakaoSdk.init(this, BuildConfig.KAKAO_API_KEY)
+        Log.e("keyHash", Utility.getKeyHash(this) )
     }
 }
--- app/src/main/java/com/depromeet/sloth/ui/LoginActivity.kt
+++ app/src/main/java/com/depromeet/sloth/ui/LoginActivity.kt
@@ -1,22 +1,55 @@
 package com.depromeet.sloth.ui
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import com.depromeet.sloth.R
 import com.depromeet.sloth.databinding.ActivityLoginBinding
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.kakao.sdk.auth.model.OAuthToken
 import com.kakao.sdk.auth.model.Prompt
 import com.kakao.sdk.user.UserApiClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import android.content.Intent
+import androidx.activity.result.ActivityResultLauncher
+import com.depromeet.sloth.BuildConfig
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.tasks.Task
+import com.google.android.gms.common.api.ApiException
+import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult
+
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
 
+    private lateinit var mGoogleSignInClient: GoogleSignInClient
+
+    private lateinit var loginLauncher: ActivityResultLauncher<Intent>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        loginLauncher = registerForActivityResult(
+            StartActivityForResult())
+            { result ->
+                if (result.resultCode == Activity.RESULT_OK) {
+                    val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+                    handleSignInResult(task)
+                }
+            }
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(BuildConfig.GOOGLE_CLIENT_ID)
+            .requestEmail()
+            .build()
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
 
         binding.kakaoLogin.setOnClickListener {
             when (UserApiClient.instance.isKakaoTalkLoginAvailable(this)) {
@@ -24,7 +57,14 @@ class LoginActivity : AppCompatActivity() {
                 false -> loginWithKakaoAccount()
             }
         }
+
+        binding.googleLogin.setOnClickListener { view ->
+            when(view.id) {
+                R.id.google_login -> loginWithGoogle()
+            }
+        }
     }
+
 
     private fun loginWithKakaoTalk() {
         UserApiClient.instance.loginWithKakaoTalk(
@@ -50,6 +90,40 @@ class LoginActivity : AppCompatActivity() {
                 Log.e("로그인 성공 -> accessToken ", token.toString())
                 Toast.makeText(this, "로그인에 성공하였습니다.", Toast.LENGTH_SHORT).show()
             }
+        }
+    }
+
+    private fun loginWithGoogle() {
+        val signInIntent: Intent = mGoogleSignInClient.signInIntent
+        loginLauncher.launch(signInIntent)
+
+    }
+
+    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+        try {
+            val account = completedTask.getResult(ApiException::class.java)
+            val idToken = account?.idToken.toString()
+            val email = account?.email.toString()
+            val displayName = account?.displayName.toString()
+
+            Log.d("idToken", idToken)
+            Log.d("account", email)
+            Log.d("displayName", displayName)
+        } catch (e: ApiException) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.e("로그인 실패", "signInResult:failed code=" + e.statusCode)
+        }
+    }
+
+
+    override fun onStart() {
+        super.onStart()
+        val gsa = GoogleSignIn.getLastSignedInAccount(this)
+
+        //기존 로그인 사용자 확인
+        if (gsa != null) {
+            //updateUI(account)
         }
     }
 }
