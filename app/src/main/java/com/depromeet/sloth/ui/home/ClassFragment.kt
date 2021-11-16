3aecaf2f439e51e3f554b053627f4a266d913d4b
iron <satls34@naver.com>
2021-11-17 01:27:04 +0900

    #18 design: ClassFragment 화면 구성 중 예정된 강의, 지난 강의 추가

--- app/src/main/java/com/depromeet/sloth/ui/home/ClassFragment.kt
+++ app/src/main/java/com/depromeet/sloth/ui/home/ClassFragment.kt
@@ -7,8 +7,10 @@ import com.depromeet.sloth.data.db.PreferenceManager
 import com.depromeet.sloth.data.network.home.LessonResponse
 import com.depromeet.sloth.databinding.FragmentClassBinding
 import com.depromeet.sloth.ui.base.BaseFragment
+import java.text.SimpleDateFormat
+import java.util.*
 
-class ClassFragment: BaseFragment<LessonViewModel, FragmentClassBinding>() {
+class ClassFragment : BaseFragment<LessonViewModel, FragmentClassBinding>() {
     private val preferenceManager: PreferenceManager by lazy { PreferenceManager(requireActivity()) }
 
     override val viewModel: LessonViewModel
@@ -20,64 +22,80 @@ class ClassFragment: BaseFragment<LessonViewModel, FragmentClassBinding>() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        var lessonList = listOf<LessonResponse>()
-
-        mainScope {
-
-        }
-
         setTestData()
     }
 
     private fun setTestData() {
         val dummyList = listOf<LessonResponse>(
             LessonResponse(
-                lessonName = "프로그래밍 시작하기 : \n파이썬 초급 (Inflearn Original)",
-                categoryName = "개발",
-                currentProgressRate = 1,
-                goalProgressRate = 4,
-                remainDay = 9,
-                isFinished = false
-            ),
-            LessonResponse(
-                lessonName = "프로그래밍 시작하기 : \n파이썬 중급 (Inflearn Original)",
-                categoryName = "디자인",
-                currentProgressRate = 0,
-                goalProgressRate = 5,
-                remainDay = 19,
-                isFinished = false
-            ),
-            LessonResponse(
-                lessonName = "프로그래밍 시작하기 : \n파이썬 고급 (Inflearn Original)",
-                categoryName = "기획",
-                currentProgressRate = 1,
-                goalProgressRate = 4,
-                remainDay = 10,
-                isFinished = false
-            ),
-            LessonResponse(
-                lessonName = "프로그래밍 시작하기 : \n파이썬 초급 (Inflearn Original)",
-                categoryName = "개발",
-                currentProgressRate = 3,
-                goalProgressRate = 3,
-                remainDay = 7,
-                isFinished = true
-            ),
-            LessonResponse(
-                lessonName = "프로그래밍 시작하기 : \n파이썬 중급 (Inflearn Original)",
-                categoryName = "디자인",
-                currentProgressRate = 6,
-                goalProgressRate = 6,
-                remainDay = 11,
-                isFinished = true
-            ),
-            LessonResponse(
-                lessonName = "프로그래밍 시작하기 : \n파이썬 고급 (Inflearn Original)",
+                lessonName = "프로그래밍 시작하기 : \nScala 고급 (Inflearn Original)",
                 categoryName = "기획",
                 currentProgressRate = 8,
                 goalProgressRate = 8,
-                remainDay = 1,
-                isFinished = true
+                totalNumber = 60,
+                price = 50000,
+                startDate = "2021-08-19 10:12:14",
+                endDate = "2021-11-24 10:12:14"
+            ),
+            LessonResponse(
+                lessonName = "프로그래밍 시작하기 : \nKotlin 고급 (Inflearn Original)",
+                categoryName = "기획",
+                currentProgressRate = 0,
+                goalProgressRate = 20,
+                totalNumber = 50,
+                price = 30000,
+                startDate = "2021-12-22 10:12:14",
+                endDate = "2022-11-11 10:12:14"
+            ),
+            LessonResponse(
+                lessonName = "프로그래밍 시작하기 : \nC++ 고급 (Inflearn Original)",
+                categoryName = "기획",
+                currentProgressRate = 0,
+                goalProgressRate = 12,
+                totalNumber = 22,
+                price = 150000,
+                startDate = "2022-01-12 10:12:14",
+                endDate = "2022-11-24 10:12:14"
+            ),
+            LessonResponse(
+                lessonName = "프로그래밍 시작하기 : \nPython 고급 (Inflearn Original)",
+                categoryName = "기획",
+                currentProgressRate = 6,
+                goalProgressRate = 8,
+                totalNumber = 16,
+                price = 25000,
+                startDate = "2021-11-10 10:12:14",
+                endDate = "2021-12-24 10:12:14"
+            ),
+            LessonResponse(
+                lessonName = "프로그래밍 시작하기 : \nGolang 고급 (Inflearn Original)",
+                categoryName = "기획",
+                currentProgressRate = 8,
+                goalProgressRate = 8,
+                totalNumber = 42,
+                price = 55000,
+                startDate = "2021-09-19 10:12:14",
+                endDate = "2021-10-22 10:12:14"
+            ),
+            LessonResponse(
+                lessonName = "프로그래밍 시작하기 : \nRuby 고급 (Inflearn Original)",
+                categoryName = "기획",
+                currentProgressRate = 4,
+                goalProgressRate = 4,
+                totalNumber = 40,
+                price = 68000,
+                startDate = "2021-11-15 10:12:14",
+                endDate = "2021-11-30 10:12:14"
+            ),
+            LessonResponse(
+                lessonName = "프로그래밍 시작하기 : \nJava 고급 (Inflearn Original)",
+                categoryName = "기획",
+                currentProgressRate = 1,
+                goalProgressRate = 4,
+                totalNumber = 30,
+                price = 33000,
+                startDate = "2021-10-19 10:12:14",
+                endDate = "2021-12-31 10:12:14"
             )
         )
 
@@ -91,22 +109,48 @@ class ClassFragment: BaseFragment<LessonViewModel, FragmentClassBinding>() {
 
         val concatAdapter = ConcatAdapter(
             doingHeader,
-            doingLessonAdapter
-            //planningHeader,
-            //planningLessonAdapter,
-            //passedHeader,
-            //passedLessonAdapter
+            doingLessonAdapter,
+            planningHeader,
+            planningLessonAdapter,
+            passedHeader,
+            passedLessonAdapter
         )
 
         dummyList.let {
             doingLessonAdapter.submitList(
-                dummyList.filter { it.isFinished }
+                dummyList.filter {
+                    val startDateString = it.startDate
+                    val endDateString = it.endDate
+                    val dateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+                    val startDate = dateFormat.parse(startDateString)
+                    val endDate = dateFormat.parse(endDateString)
+                    val todayDate = Calendar.getInstance()
+                    val startDiffTime = (startDate.time - todayDate.time.time) / (60 * 60 * 24 * 1000).toDouble()
+                    val endDiffTime = (endDate.time - todayDate.time.time) / (60 * 60 * 24 * 1000).toDouble()
+                    val progressRatio = it.currentProgressRate / it.goalProgressRate.toDouble()
+                    val inValidData = startDiffTime < 0.0 && endDiffTime >= 0.0 && progressRatio < 1.0
+                    inValidData
+                }
             )
+
             planningLessonAdapter.submitList(
-                dummyList
+                dummyList.filter {
+                    val startDateString = it.startDate
+                    val dateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+                    val startDate = dateFormat.parse(startDateString)
+                    val todayDate = Calendar.getInstance()
+                    val endDiffTime = (startDate.time - todayDate.time.time) / (60 * 60 * 24 * 1000).toDouble()
+                    val inValidData = endDiffTime > 0.0
+                    inValidData
+                }
             )
+
             passedLessonAdapter.submitList(
-                dummyList.filter { it.isFinished.not() }
+                dummyList.filter {
+                    val progressRatio = it.currentProgressRate / it.goalProgressRate
+                    val inValidData = (progressRatio == 1)
+                    inValidData
+                }
             )
         }
 
--- app/src/main/java/com/depromeet/sloth/ui/home/ClassLessonAdapter.kt
+++ app/src/main/java/com/depromeet/sloth/ui/home/ClassLessonAdapter.kt
@@ -1,15 +1,13 @@
 package com.depromeet.sloth.ui.home
 
-import android.animation.Animator
 import android.animation.ObjectAnimator
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.animation.AccelerateInterpolator
 import android.view.animation.DecelerateInterpolator
 import android.widget.ProgressBar
 import android.widget.TextView
-import androidx.appcompat.widget.AppCompatTextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.constraintlayout.widget.ConstraintSet
 import androidx.core.animation.addListener
@@ -18,6 +16,7 @@ import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.depromeet.sloth.R
 import com.depromeet.sloth.data.network.home.LessonResponse
+import kotlin.math.ceil
 
 class ClassLessonAdapter(
     private val bodyType: BodyType
@@ -57,40 +56,64 @@ class ClassLessonAdapter(
         private val bodyType: BodyType,
         itemView: View
     ) : RecyclerView.ViewHolder(itemView) {
-        private val classLessonLayout =
-            itemView.findViewById<ConstraintLayout>(R.id.cl_class_lesson_layoutc)
-        private val classLessonGoalLine =
-            itemView.findViewById<View>(R.id.vw_class_lesson_goal_line)
-        private val classLessonGoalText =
-            itemView.findViewById<TextView>(R.id.tv_class_lesson_goal_text)
+        private val classLessonCategory = itemView.findViewById<TextView>(R.id.tv_class_lesson_category)
+        private val classLessonName = itemView.findViewById<TextView>(R.id.tv_class_lesson_name)
+        private val classLessonPrice = itemView.findViewById<TextView>(R.id.tv_class_lesson_price)
+        private val classLessonLayout = itemView.findViewById<ConstraintLayout>(R.id.cl_class_lesson_layoutc)
+        private val classLessonGoalLine = itemView.findViewById<View>(R.id.vw_class_lesson_goal_line)
+        private val classLessonGoal = itemView.findViewById<TextView>(R.id.tv_class_lesson_goal)
+        private val classLessonGoalGroup = itemView.findViewById<ConstraintLayout>(R.id.cl_class_goal_group)
         private val classLessonBar = itemView.findViewById<ProgressBar>(R.id.pb_class_lesson_bar)
+        private val classLessonPercent = itemView.findViewById<TextView>(R.id.tv_class_lesson_percent)
+        private val classLessonCurrentNumber = itemView.findViewById<TextView>(R.id.tv_class_lesson_current_number)
+        private val classLessonTotalNumber = itemView.findViewById<TextView>(R.id.tv_class_lesson_total_number)
+        private val classLessonPlanningDate = itemView.findViewById<TextView>(R.id.tv_class_lesson_planning_date)
+        private val classLessonPassedCount = itemView.findViewById<TextView>(R.id.tv_class_lesson_total_count)
 
         fun onBind(lesson: LessonResponse) {
+            classLessonCategory.text = lesson.categoryName
+            classLessonName.text = lesson.lessonName
+            classLessonPrice.text = lesson.price.toString()
+
             when (bodyType) {
                 BodyType.DOING -> {
+                    val progressRate = lesson.currentProgressRate / lesson.totalNumber.toFloat()
+                    classLessonCurrentNumber.text = lesson.currentProgressRate.toString()
+                    classLessonTotalNumber.text = lesson.totalNumber.toString()
+                    classLessonPercent.text = ceil((100 - (progressRate * 100)).toDouble()).toInt().toString()
+                    classLessonGoal.text = (progressRate * 100).toInt().toString()
+
                     val set = ConstraintSet()
                     set.clone(classLessonLayout)
-                    set.setHorizontalBias(classLessonGoalLine.id, 0.4f)
+                    set.setHorizontalBias(classLessonGoalLine.id, progressRate)
                     set.applyTo(classLessonLayout)
                     classLessonGoalLine.bringToFront()
 
-                    classLessonBar.max = 10000
                     val animation = ObjectAnimator.ofInt(
                         classLessonBar,
                         "progress",
                         classLessonBar.progress,
-                        4000
+                        (progressRate * 10000).toInt()
                     )
+                    Log.e("test2", (ceil(progressRate.toDouble()) * 10000).toInt().toString())
                     animation.apply {
                         addListener(
                             onEnd = {
-                                classLessonGoalText.visibility = View.VISIBLE
-                                classLessonGoalText.bringToFront()
+                                classLessonGoalGroup.visibility = View.VISIBLE
+                                classLessonGoalGroup.bringToFront()
                             }
                         )
                         duration = 500
                         interpolator = DecelerateInterpolator()
                     }.start()
+                }
+
+                BodyType.PLANNING -> {
+                    classLessonPlanningDate.text = lesson.startDate.split(" ")[0]
+                }
+
+                BodyType.PASSED -> {
+                    classLessonPassedCount.text = lesson.totalNumber.toString()
                 }
             }
         }
