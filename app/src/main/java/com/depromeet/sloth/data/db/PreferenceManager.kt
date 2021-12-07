6e8272fef05f588979d3bba58e34d102b931bfae
easyhooon <51016231+easyhooon@users.noreply.github.com>
2021-12-07 22:28:12 +0900

    refactor: PreferenceManager 사용하지 않는 메소드, 변수 제거, AuthToken 제거 메소드 추가

--- app/src/main/java/com/depromeet/sloth/data/db/PreferenceManager.kt
+++ app/src/main/java/com/depromeet/sloth/data/db/PreferenceManager.kt
@@ -13,12 +13,6 @@ class PreferenceManager(
     companion object {
         const val PREFERENCES_NAME = "Sloth-pref"
         private const val DEFAULT_VALUE_STRING = ""
-        private const val DEFAULT_VALUE_BOOLEAN = false
-        private const val DEFAULT_VALUE_INT = -1
-        private const val DEFAULT_VALUE_LONG = -1L
-        private const val DEFAULT_VALUE_FLOAT = -1f
-
-        const val ID_TOKEN = "idToken"
         const val ACCESS_TOKEN = "accessToken"
         const val REFRESH_TOKEN = "refreshToken"
     }
@@ -32,110 +26,6 @@ class PreferenceManager(
     private val editor by lazy { prefs.edit() }
 
     /**
-     * String 값 저장
-     * @param key
-     * @param value
-     */
-    fun setString(key: String?, value: String?) {
-        editor.putString(key, value)
-        editor.apply()
-    }
-
-    /**
-     * boolean 값 저장
-     * @param key
-     * @param value
-     */
-    fun setBoolean(key: String?, value: Boolean) {
-        editor.putBoolean(key, value)
-        editor.apply()
-    }
-
-    /**
-     * int 값 저장
-     * @param key
-     * @param value
-     */
-    fun setInt(key: String?, value: Int) {
-        editor.putInt(key, value)
-        editor.apply()
-    }
-
-    /**
-     * long 값 저장
-     * @param key
-     * @param value
-     */
-    fun setLong(key: String?, value: Long) {
-        editor.putLong(key, value)
-        editor.apply()
-    }
-
-    /**
-     * float 값 저장
-     * @param key
-     * @param value
-     */
-    fun setFloat(key: String?, value: Float) {
-        editor.putFloat(key, value)
-        editor.apply()
-    }
-
-    /**
-     * String 값 로드
-     * @param key
-     * @return
-     */
-    fun getString(key: String?): String? {
-        return prefs.getString(key, DEFAULT_VALUE_STRING)
-    }
-
-    /**
-     * boolean 값 로드
-     * @param key
-     * @return
-     */
-    fun getBoolean(key: String?): Boolean {
-        return prefs.getBoolean(key, DEFAULT_VALUE_BOOLEAN)
-    }
-
-    /**
-     * int 값 로드
-     * @param key
-     * @return
-     */
-    fun getInt(key: String?): Int {
-        return prefs.getInt(key, DEFAULT_VALUE_INT)
-    }
-
-    /**
-     * long 값 로드
-     * @param key
-     * @return
-     */
-    fun getLong(key: String?): Long {
-        return prefs.getLong(key, DEFAULT_VALUE_LONG)
-    }
-
-    /**
-     * float 값 로드
-     * @param key
-     * @return
-     */
-    fun getFloat(key: String?): Float {
-        return prefs.getFloat(key, DEFAULT_VALUE_FLOAT)
-    }
-
-    /**
-     * 키 값 삭제
-     * @param key
-     */
-    fun removeKey(key: String?) {
-        editor.remove(key)
-        editor.apply()
-    }
-
-    /**
      * 모든 저장 데이터 삭제
      */
     fun clear() {
@@ -143,14 +33,6 @@ class PreferenceManager(
         editor.apply()
     }
 
-    /**
-     * accessToken 저장
-     * @param accessToken
-     */
-    fun putAccessToken(accessToken: String) {
-        editor.putString(ACCESS_TOKEN, accessToken)
-        editor.apply()
-    }
 
     /**
      * accessToken, refreshToken (인증 토큰) 저장
@@ -182,12 +64,13 @@ class PreferenceManager(
         return prefs.getString(REFRESH_TOKEN, null)
     }
 
-    fun getIdToken(): String? {
-        return prefs.getString(ID_TOKEN, null)
-    }
-
-    fun removeIdToken() {
-        editor.putString(ID_TOKEN, null)
+    /**
+     * 저장된 AccessToken, RefreshToken (인증 토큰) 삭제
+     *
+     */
+    fun removeAuthToken(accessToken: String, refreshToken: String) {
+        editor.remove(accessToken)
+        editor.remove(refreshToken)
         editor.apply()
     }
 }
