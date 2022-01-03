4a23bd2f63799094f0f1703084a661048725c05b
easyhooon <51016231+easyhooon@users.noreply.github.com>
2022-01-03 10:06:41 +0900

    design: 로딩 다이얼로그로 인한 화면이 딤 처리되는 효과 제거

--- app/src/main/java/com/depromeet/sloth/ui/custom/LoadingDialog.kt
+++ app/src/main/java/com/depromeet/sloth/ui/custom/LoadingDialog.kt
@@ -4,6 +4,7 @@ import android.app.Dialog
 import android.content.Context
 import android.graphics.drawable.ColorDrawable
 import android.view.Window
+import android.view.WindowManager
 import com.depromeet.sloth.R
 
 class LoadingDialog(
@@ -12,6 +13,7 @@ class LoadingDialog(
     init {
         requestWindowFeature(Window.FEATURE_NO_TITLE)
         window?.setBackgroundDrawable(ColorDrawable(android.graphics.Color.TRANSPARENT))
+        window?.clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND)
         setContentView(R.layout.view_loading_dialog)
         setCanceledOnTouchOutside(false)
     }
