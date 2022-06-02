4e7b259dce909acd8ead3a734ab613a75991506e
easyhooon <51016231+easyhooon@users.noreply.github.com>
2022-06-02 10:37:35 +0900

    fix: BindingAdapter 클래스 하나로 통일

--- app/src/main/java/com/depromeet/sloth/ui/common/BindingAdapters.kt
+++ app/src/main/java/com/depromeet/sloth/ui/common/BindingAdapters.kt
@@ -1,14 +1,32 @@
 package com.depromeet.sloth.ui.common
 
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.content.res.AppCompatResources
 import androidx.databinding.BindingAdapter
+import com.depromeet.sloth.GlideApp
 import com.depromeet.sloth.R
 import com.depromeet.sloth.extensions.changeDateFormatToDot
 import com.depromeet.sloth.extensions.changeListToDot
 import com.skydoves.progressview.ProgressView
 import java.text.DecimalFormat
+
+@BindingAdapter("currentProgressRate", "goalProgressRate")
+fun setLessonSummaryImage(view: ImageView, currentProgressRate: Float, goalProgressRate: Float) {
+    // 시작 하지 않음
+    if(goalProgressRate == 0F) {
+        GlideApp.with(view.context).load(R.drawable.ic_detail_sloth_not_started_yet).into(view)
+    }
+    // 시작함
+    else {
+        if (currentProgressRate >= goalProgressRate) {
+            GlideApp.with(view.context).load(R.drawable.ic_detail_sloth_steadily_listen).into(view)
+        } else {
+            GlideApp.with(view.context).load(R.drawable.ic_detail_sloth_fail_goal).into(view)
+        }
+    }
+}
 
 @BindingAdapter("isVisible")
 fun isVisible(view: TextView, goalProgressRate: Float) = with(view) {
@@ -150,5 +168,6 @@ fun setLessonPeriod(view: TextView, startDate: ArrayList<String>?, endDate: Arra
             startDate?.let { changeDateFormatToDot(it) },
             endDate?.let { changeDateFormatToDot(it) })
     }
+
 
 
