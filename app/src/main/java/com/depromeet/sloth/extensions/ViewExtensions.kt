03086f24807bf655effb861ae76b6c692bba03d8
easyhooon <mraz3068@gmail.com>
2022-11-04 13:18:44 +0900

    remove: 사용하지 않는 LiveData 의존성 제거

--- app/src/main/java/com/depromeet/sloth/extensions/ViewExtensions.kt
+++ app/src/main/java/com/depromeet/sloth/extensions/ViewExtensions.kt
@@ -4,13 +4,6 @@ import androidx.lifecycle.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
-
-fun <T> MediatorLiveData<T>.addSourceList(vararg liveDataArgument: MutableLiveData<*>, onChanged: () -> T) {
-    liveDataArgument.forEach {
-        this.addSource(it) { value = onChanged() }
-    }
-}
-
 fun LifecycleOwner.repeatOnStarted(block: suspend CoroutineScope.() -> Unit) {
     lifecycleScope.launch {
         lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED, block)
