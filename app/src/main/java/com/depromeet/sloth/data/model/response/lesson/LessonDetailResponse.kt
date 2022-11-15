15aed5e2bde6eba604ce80391c4719afb0a1d17d
easyhooon <mraz3068@gmail.com>
2022-11-15 10:34:57 +0900

    fix: response data class @Keep annotation 제거
    SerializedName 을 모두 붙혀줌으로써 난독화 방지

--- app/src/main/java/com/depromeet/sloth/data/model/response/lesson/LessonDetailResponse.kt
+++ app/src/main/java/com/depromeet/sloth/data/model/response/lesson/LessonDetailResponse.kt
@@ -1,11 +1,9 @@
 package com.depromeet.sloth.data.model.response.lesson
 
 import android.os.Parcelable
-import androidx.annotation.Keep
 import com.google.gson.annotations.SerializedName
 import kotlinx.parcelize.Parcelize
 
-@Keep
 @Parcelize
 data class LessonDetailResponse (
     @SerializedName("alertDays")
