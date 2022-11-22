805acfbb3908d40633e1851a3034337104298fc7
easyhooon <mraz3068@gmail.com>
2022-11-23 02:06:14 +0900

    fix: fetchFCMToken 함수 앱 실행시에만 호출되도록 변경
    사용하지 않는 함수 제거

--- app/src/main/java/com/depromeet/sloth/di/DataModule.kt
+++ app/src/main/java/com/depromeet/sloth/di/DataModule.kt
@@ -6,25 +6,15 @@ import android.content.SharedPreferences
 import com.depromeet.sloth.data.preferences.Preferences
 import com.depromeet.sloth.data.preferences.PreferencesImpl
 import com.depromeet.sloth.util.KEY_PREFERENCES
-import dagger.Provides
-import dagger.hilt.android.qualifiers.ApplicationContext
-import javax.inject.Singleton
-import android.provider.Settings
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object DataModule {
-
-    @Provides
-    @Singleton
-    fun provideDeviceId(
-        @ApplicationContext context: Context
-    ): String {
-        return Settings.Secure.getString(context.contentResolver, Settings.Secure.ANDROID_ID)
-    }
 
     @Provides
     @Singleton
