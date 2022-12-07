7ba1924bc36754c55d1790362216af5a3c14b705
easyhooon <mraz3068@gmail.com>
2022-12-07 15:00:27 +0900

    fix: fcm 토큰 등록 로직 변경
    앱 진입할때마다 fcm 토큰을 서버에 전달하는 방식으로 로직 변경, 로컬 DB에 fcm 토큰을 저장하고, 불러오는 함수 제거

--- app/src/main/java/com/depromeet/sloth/ui/home/HomeActivity.kt
+++ app/src/main/java/com/depromeet/sloth/ui/home/HomeActivity.kt
@@ -8,12 +8,11 @@ import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.depromeet.sloth.R
-import com.depromeet.sloth.data.model.response.notification.NotificationFetchResponse
+import com.depromeet.sloth.common.Result
 import com.depromeet.sloth.databinding.ActivityHomeBinding
 import com.depromeet.sloth.extensions.repeatOnStarted
 import com.depromeet.sloth.extensions.showForbiddenDialog
 import com.depromeet.sloth.ui.base.BaseActivity
-import com.depromeet.sloth.common.Result
 import com.depromeet.sloth.util.LoadingDialogUtil.hideProgress
 import com.depromeet.sloth.util.LoadingDialogUtil.showProgress
 import dagger.hilt.android.AndroidEntryPoint
@@ -28,7 +27,6 @@ class HomeActivity : BaseActivity<ActivityHomeBinding>(R.layout.activity_home) {
 
     private lateinit var navController: NavController
 
-    //TODO Datasource 로 옮기기
     private val deviceId: String by lazy {
         Settings.Secure.getString(contentResolver, Settings.Secure.ANDROID_ID)
     }
@@ -36,11 +34,9 @@ class HomeActivity : BaseActivity<ActivityHomeBinding>(R.layout.activity_home) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // 최초 실행시에만 호출 - 이렇게 간단하게 해결해줄 수도 있음
-        // preference 에 fcmToken 을 저장을 해서 있나 없나만 확인하는 방법을 사용하면 앱을 실행할때마다 fetch Api를 호출할 필요가 없게됨
-        // 로그아웃할때 preference 에 fcm 토큰 제거해주면 다중 로그인문제도 해결
+        // 앱 진입시 fcm 토큰을 서버에 전달하는 방식으로 로직 변경
         if (savedInstanceState == null) {
-            homeViewModel.fetchFCMToken(deviceId)
+            homeViewModel.createAndRegisterFCMToken(deviceId)
         }
 
         initNavigation()
@@ -60,31 +56,6 @@ class HomeActivity : BaseActivity<ActivityHomeBinding>(R.layout.activity_home) {
 
     private fun initObserver() = with(homeViewModel) {
         repeatOnStarted {
-            launch {
-                notificationFetchState
-                    .collect { result ->
-                        when (result) {
-                            is Result.Loading -> showProgress(this@HomeActivity)
-                            is Result.UnLoading -> hideProgress()
-                            is Result.Success<NotificationFetchResponse> -> {
-                                if (result.data.fcmToken == null) {
-                                    Timber.d("fcmToken not existed")
-                                    createAndRegisterFCMToken(deviceId)
-                                } else {
-                                    Timber.d("fcmToken already existed")
-                                }
-                            }
-                            is Result.Error ->
-                                when (result.statusCode) {
-                                    401 -> {
-                                        showForbiddenDialog(this@HomeActivity) { homeViewModel.removeAuthToken() }
-                                    }
-                                    else -> Timber.tag("Fetch Error").d(result.throwable)
-                                }
-                        }
-                    }
-            }
-
             launch {
                 notificationRegisterState
                     .collect { result ->
