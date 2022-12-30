29ef4240979e0aeb7c67a8443672c74fe30d93e1
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2022-12-30 15:54:15 +0900

    feat: DebounceEditListener 추가
    EditTextState sealed class, cancelIfActive extension function 추가, flowExtensions -> CoroutineExtensions 파일로 이름 변경

--- app/src/main/java/com/depromeet/sloth/extensions/CoroutineExtensions.kt
+++ app/src/main/java/com/depromeet/sloth/extensions/CoroutineExtensions.kt
@@ -2,6 +2,7 @@ package com.depromeet.sloth.extensions
 
 import androidx.lifecycle.*
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 
 fun LifecycleOwner.repeatOnStarted(block: suspend CoroutineScope.() -> Unit) {
@@ -10,3 +11,8 @@ fun LifecycleOwner.repeatOnStarted(block: suspend CoroutineScope.() -> Unit) {
     }
 }
 
+fun Job?.cancelIfActive() {
+    if (this?.isActive == true) {
+        cancel()
+    }
+}
