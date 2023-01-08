8ad3d05a0207f3358a704ba3558123d93668240a
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2023-01-09 03:48:54 +0900

    feat: BaseViewModel 내에 로딩 상태를 처리할 변수 및 상태 변경 함수 추가

--- app/src/main/java/com/depromeet/sloth/ui/base/BaseViewModel.kt
+++ app/src/main/java/com/depromeet/sloth/ui/base/BaseViewModel.kt
@@ -1,8 +1,19 @@
 package com.depromeet.sloth.ui.base
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 abstract class BaseViewModel: ViewModel() {
 
+    private val _isLoading = MutableStateFlow<Boolean>(false)
+    val isLoading: StateFlow<Boolean> = _isLoading.asStateFlow()
+
+
     abstract fun retry()
+
+    fun setLoading(isLoading: Boolean) {
+        _isLoading.value = isLoading
+    }
 }
