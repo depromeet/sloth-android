4576a2a832fc8e8d3a6e5212c15349524894ef06
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2023-02-05 23:15:20 +0900

    fix: CheckLoggedInUseCase 네이밍 변경
    UseCase 네이밍 컨벤션에 맞춰 FetchLoginStatusUseCase 로 네이밍 변경

--- app/src/main/java/com/depromeet/sloth/domain/usecase/login/FetchLoginStatusUseCase.kt
+++ app/src/main/java/com/depromeet/sloth/domain/usecase/login/FetchLoginStatusUseCase.kt
@@ -3,7 +3,7 @@ package com.depromeet.sloth.domain.usecase.login
 import com.depromeet.sloth.domain.repository.LoginRepository
 import javax.inject.Inject
 
-class CheckLoggedInUseCase @Inject constructor(
+class FetchLoginStatusUseCase @Inject constructor(
     private val loginRepository: LoginRepository
 ) {
     suspend operator fun invoke(): Boolean {
