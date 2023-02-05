854725b635f85f7b3f6b93648b54a428315f6187
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2023-02-05 23:17:03 +0900

    fix: GetOnBoardingStateUseCase 네이밍 변경
    UseCase 네이밍 컨벤션에 맞춰 FetchOnBoardingStatusUseCase 로 네이밍 변경

--- app/src/main/java/com/depromeet/sloth/domain/usecase/member/FetchOnBoardingStatusUseCase.kt
+++ app/src/main/java/com/depromeet/sloth/domain/usecase/member/FetchOnBoardingStatusUseCase.kt
@@ -3,7 +3,7 @@ package com.depromeet.sloth.domain.usecase.member
 import com.depromeet.sloth.domain.repository.MemberRepository
 import javax.inject.Inject
 
-class GetOnBoardingStateUseCase @Inject constructor(
+class FetchOnBoardingStatusUseCase @Inject constructor(
     private val memberRepository: MemberRepository
 ) {
     suspend operator fun invoke() : Boolean {
