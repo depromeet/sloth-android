05291301772d4174939456e9820fabf8ae7c315e
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2023-03-16 04:49:32 +0900

    fix: compileSDK, targetSDK upgrade
    광고 ID 관련 퍼미션 추가

--- buildSrc/src/main/java/ProjectConfig.kt
+++ buildSrc/src/main/java/ProjectConfig.kt
@@ -1,7 +1,7 @@
 object ProjectConfig {
-    const val compileSdk = 32
+    const val compileSdk = 33
     const val minSdk = 24
-    const val targetSdk = 32
+    const val targetSdk = 33
     const val versionCode = 29
     const val versionName = "1.1.2"
 }
