fdbc2f83d474ed4d4a690096f276c257fcb094b9
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2023-03-16 04:33:21 +0900

    build: 릴리즈 1.1.2버전 프로덕션 출시 준비 완료
    투데이 화면 강의의 부가 설명 추가
    알림 목록 화면 받은 알림이 없을 경우 에러가 출력되었던 부분 해결
    투데이화면, 온보딩화면에서 강의 등록 후 로그아웃 했을 경우 백스택에 모든 화면들이 남아 있던 문제 해결
    강의 목록 화면에서 종료된 강의에 대해서는 상세 화면으로 이동하지 않도록 로직 수정

--- buildSrc/src/main/java/ProjectConfig.kt
+++ buildSrc/src/main/java/ProjectConfig.kt
@@ -2,6 +2,6 @@ object ProjectConfig {
     const val compileSdk = 33
     const val minSdk = 24
     const val targetSdk = 33
-    const val versionCode = 28
-    const val versionName = "1.1.1"
+    const val versionCode = 29
+    const val versionName = "1.1.2"
 }
