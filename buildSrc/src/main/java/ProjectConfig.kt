277e63f35fecae8678dde3fc067addc269bcc5d4
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2023-03-16 17:46:05 +0900

    build: 릴리즈 1.1.7버전 프로덕션 출시 준비 완료

--- buildSrc/src/main/java/ProjectConfig.kt
+++ buildSrc/src/main/java/ProjectConfig.kt
@@ -2,6 +2,6 @@ object ProjectConfig {
     const val compileSdk = 33
     const val minSdk = 24
     const val targetSdk = 33
-    const val versionCode = 33
-    const val versionName = "1.1.6"
+    const val versionCode = 34
+    const val versionName = "1.1.7"
 }
