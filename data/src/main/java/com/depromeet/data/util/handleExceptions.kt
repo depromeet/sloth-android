e8aaf186691f4c0aff005c16a6be4ee3053281e1
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2023-04-26 10:53:09 +0900

    fix: Exception 핸들링 함수 수정
    서버 에러 -> SocketTimeoutException
    인터넷 연경 에러 -> ConnectException

--- data/src/main/java/com/depromeet/data/util/handleExceptions.kt
+++ data/src/main/java/com/depromeet/data/util/handleExceptions.kt
@@ -1,20 +1,20 @@
 package com.depromeet.data.util
 
+import com.depromeet.domain.util.Result
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
-import java.io.IOException
 import java.net.ConnectException
-import com.depromeet.domain.util.Result
+import java.net.SocketTimeoutException
 
 fun <T> Flow<Result<T>>.handleExceptions(): Flow<Result<T>> {
     return this.catch { throwable ->
         when (throwable) {
-            is ConnectException -> {
-                // Handle Timeout Error
+            is SocketTimeoutException -> {
+                // Server Connection Error
                 emit(Result.Error(Exception(SERVER_CONNECTION_ERROR)))
             }
-            is IOException -> {
-                // Handle Other Network Error
+            is ConnectException -> {
+                // Internet Connection Error
                 emit(Result.Error(Exception(INTERNET_CONNECTION_ERROR)))
             }
             else -> {
