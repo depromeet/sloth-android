65a34491d50d068420abf1e027d0d2a0a58b8018
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2023-05-10 14:38:33 +0900

    feat: project 수준 build.gradle kotlin-jvm 라이브러리 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.kotlin.parcelize) apply false
     alias(libs.plugins.kotlinx.serialization) apply false
     alias(libs.plugins.navigation.safeargs) apply false
