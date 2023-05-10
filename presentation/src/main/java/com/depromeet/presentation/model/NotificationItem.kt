1d0a4f8c2f8b46d058bb326b9876918c7d88f94f
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2023-05-10 17:58:31 +0900

    fix:NotificationItem seald class 파일 네이밍 변경

--- presentation/src/main/java/com/depromeet/presentation/model/NotificationItem.kt
+++ presentation/src/main/java/com/depromeet/presentation/model/NotificationItem.kt
@@ -1,3 +1,19 @@
 package com.depromeet.presentation.model
 
-//interface NotificationItem
+
+sealed class NotificationItem {
+    object RestartOnBoarding : NotificationItem()
+    data class Notification(
+        val alarmContentId: Long,
+        val alarmId: Long,
+        val alarmType: String,
+        val message: String,
+        val occurrenceTime: String,
+        val readTime: String?,
+        val title: String,
+        val url: String?
+    ) : NotificationItem()
+}
+
+
+
